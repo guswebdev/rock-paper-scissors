2b89d6eddfaab22bb1f885824c528c1ce2b0a3fc
gusemmanuel <gus.desarrollo.web@gmail.com>
2024-07-03 20:10:07 -0300

    Completar la logica del juego de piedra, papel, tijera en consola

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const d = document;
-const humanScore = 0;
-const computerScore = 0;
-
-const humanSelection = getHumanChoice();
-//const computerSelection = getComputerChoice();
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3 + 1);
@@ -25,16 +22,81 @@ function getHumanChoice() {
   return transforLowerCase(prompt("Enter a choice: rock | paper | scissors"));
 }
 
-function playRound(humanChoice, computerChoice) {
-    //Paso 5: Crear la logica para jugar una ronda
-}
-
-function playGame(){
-    //Paso 6: Crear la logica para jugar 5 rondas (usar PlayRound)
-}
-
 function transforLowerCase(string) {
   return string.toLowerCase();
 }
 
-playRound(humanSelection, computerSelection);
+function playRound(humanChoice, computerChoice) {
+  console.log("HUMAN " + humanChoice);
+  
+  console.log("COMPUTER " + computerChoice);
+  
+  //Paso 5: Crear la logica para jugar una ronda
+
+  if (empate(humanChoice, computerChoice)) {
+    console.log("Hubo empate");
+  } else if (winRound(humanChoice, computerChoice)) {
+    humanScore++;
+    console.log("HUMAN WIN");
+  } else {
+    computerScore++;
+    console.log("COMPUTER WIN");
+  }
+
+  console.log(humanScore);
+  console.log(computerScore);
+}
+
+function empate(humanChoice, computerChoice) {
+  return humanChoice === computerChoice;
+}
+
+function winRound(humanChoice, computerChoice) {
+  return (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+    ? true
+    : false;
+}
+
+function showResult(humanScore, computerScore) {
+  console.log("HUMAN " + humanScore);
+  console.log("COMPUTER " + computerScore);
+}
+
+function showMessageResult(humanScore, computerScore) {
+  if (humanScore === computerScore) {
+    console.log("HUBO EMPATE");
+  } else if (humanScore > computerScore) {
+    console.log("EL HUMAN ES EL GANADOR");
+  } else {
+    console.log("EL COMPUTER ES EL GANADOR");
+  }
+}
+
+function resetCount(humanScore, computerScore) {
+  humanScore = 0;
+  computerScore = 0;
+}
+
+function playGame() {
+  //Paso 6: Crear la logica para jugar 5 rondas (usar PlayRound)
+
+  for (let i = 1; i <= 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    console.log("Round " + i);
+    playRound(humanSelection, computerSelection);
+  }
+
+  //Muestra el resultado
+  showResult(humanScore, computerScore);
+
+  //Muestra el mensaje del ganador
+  showMessageResult(humanScore, computerScore);
+
+  //reset contador
+  resetCount();
+}
+
+playGame();
